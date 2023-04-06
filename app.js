a5353efbb9dfb4d89b4610fcbe9e17f7664ab771
Anand Sharma <ak2865483@gmail.com>
2023-04-06 14:28:00 +0530

    second-commit

--- app.js
+++ app.js
@@ -131,3 +131,27 @@ function getGradient(name) {
       return "linear-gradient(180deg, #A9C9FF 0%, #FFBBEC 100%)";
   }
 }
+
+
+
+
+        
+        
+$(document).ready(function() {
+
+  $('.counter').each(function () {
+$(this).prop('Counter',0).animate({
+  Counter: $(this).text()
+}, {
+  duration: 4000,
+  easing: 'swing',
+  step: function (now) {
+      $(this).text(Math.ceil(now));
+  }
+});
+});
+
+});  
+
+
+
